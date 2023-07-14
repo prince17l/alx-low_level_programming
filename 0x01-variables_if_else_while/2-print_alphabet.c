450f44a814a7b2a18909f8676db3b98088248a5a
Leul_Mihretu <learnpro1016@gmail.com>
2023-07-14 13:44:33 -0700

    2-done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*  main - this is a main function.
+*  description - it will print a-z.
+*  Return: integer
+*/
+int main(void)
+{
+	char letter = 'a';
+    
+	while (letter <= 'z')
+	{
+	putchar(letter);
+	letter++;
+	}
+	putchar('\n');
+	return 0;
+}
