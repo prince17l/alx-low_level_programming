3e91e0ef5d5fe1b73cbab75aad12c1dcfca366b9
Leul_Mihretu <learnpro1016@gmail.com>
2023-07-15 13:41:48 -0700

    print it

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  main - this is a main function.
+ *  description - it will print some text.
+ *  Return: integer
+ */
+int main(void)
+{
+	putchar("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
