219ea8fbfd57bfb3a757af7e48fc4d60d8d4c3b1
Leul_Mihretu <learnpro1016@gmail.com>
2023-07-15 14:34:11 -0700

    print it

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  main - this is a main function.
+ *  description - it will print size of data types.
+ *  Return: integer
+ */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
