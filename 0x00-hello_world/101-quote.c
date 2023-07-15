bc36a2848c10a2afa16bc295fbd8ce0cffa3cc66
Leul_Mihretu <learnpro1016@gmail.com>
2023-07-15 14:55:33 -0700

    sdjf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  main - this is a main function.
+ *  description - it will print some text.
+ *  Return: integer
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
