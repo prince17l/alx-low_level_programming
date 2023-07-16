4d0a242dc3f354453cb8068d7d47a0066a0cc370
Leul_Mihretu <learnpro1016@gmail.com>
2023-07-16 11:12:27 -0700

    9-done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*  main - this is a main function.
+*  description - it will print 0-9 with putchar.
+*  Return: integer
+*/
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		putchar('0' + number);
+		if (number == 9)
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+			number++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
