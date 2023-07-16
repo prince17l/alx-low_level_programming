4356cc7def335d621937c596f4ef5e8a547bcaaf
Leul_Mihretu <learnpro1016@gmail.com>
2023-07-16 03:52:49 -0700

    5-done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*  main - this is a main function.
+*  description - it will print 0-9.
+*  Return: integer
+*/
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+}
