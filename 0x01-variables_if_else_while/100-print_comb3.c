959dd56a56ec7218fe638102850a9eaf5fdba19c
Leul_Mihretu <learnpro1016@gmail.com>
2023-07-19 09:33:59 -0700

    readme

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int first = 0;
+	int second = 0;
+	while (number < 100)
+	{
+		if (second % 10 == 0)
+		{
+			putchar('0''0'+
+	}
+	putchar('\n');
+	return (0);
+}
