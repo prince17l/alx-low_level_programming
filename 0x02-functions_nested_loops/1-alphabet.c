916812cc5dc25b346a604e038aca1bd04f9c5fef
Leul_Mihretu <learnpro1016@gmail.com>
2023-07-22 08:38:08 -0700

    1-done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*  main - this is a main function.
+*  description - it will print a-z.
+*  Return: integer
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 *  main - this is a main function.
-*  description - it will print single digit numbers.
+*  description - it is a header file with many functions.
 *  Return: integer
 */
 int put(void)
@@ -16,4 +16,29 @@ int put(void)
 	putchar('r');
 	putchar('\n');
 	return (0);
+}
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter != 'z' + 1)
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+}
+void print_alphabet_x10(void)
+{
+	char letter = 'a';
+	int i;
+	for (i = 0; i <10; i++)
+	{
+		while (letter != 'z' + 1)
+		{
+			putchar(letter);
+			letter;
+		}
+		putchar('\n');
+	}
 }
