83d76d462c86f1cedbc6481e04f36236c2fafcba
Leul_Mihretu <learnpro1016@gmail.com>
2023-07-31 14:34:41 -0700

    0-done

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*  *_memset - this is a _memeset function.
+*  description - it will print single digit numbers.
+*  @s: poiter to char
+*  @b: parameter with type char
+*  @n: parameter with type unsigned int
+*  Return: integer
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	int **p = &s;
+
+	for (i = 0; i < 10; i++)
+	{
+		*(s + i) = b;
+	}
+	return (p);
+}
