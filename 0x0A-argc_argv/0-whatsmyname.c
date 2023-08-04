b1e2c7d6a37143f46255811a101c0b8bc6df8fb0
Leul_Mihretu <learnpro1016@gmail.com>
2023-08-04 16:06:18 -0700

    try

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - it is a main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
