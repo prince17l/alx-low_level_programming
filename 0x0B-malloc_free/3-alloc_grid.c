28e7f29e0c7000f311c69b01cd9583d969b87276
Leul_Mihretu <learnpro1016@gmail.com>
2023-08-08 04:26:27 -0700

    done

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * alloc_grid - it is a main function
+ * @width: variable with type int
+ * @height: variable with type int
+ * Return: int
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width == 0 || height == 0 || width < 0 || height < 0)
+		return (NULL);
+	grid = malloc(sizeof(int) * width * height);
+	if (grid == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
