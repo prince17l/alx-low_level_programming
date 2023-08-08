22d1830d203a196504eb25843b95a228f2ffa03a
Leul_Mihretu <learnpro1016@gmail.com>
2023-08-08 10:40:29 -0700

    done

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,17 +1,21 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * alloc_grid - it is a main function
- * @width: variable with type int
+ * free_grid - it is a main function
+ * @grid: 2d array with type int
  * @height: variable with type int
- * Return: int
+ * Return: void
  */
 void free_grid(int **grid, int height)
 {
-	int i = 0;
+	int i, j;
+	int width;
+	int size;
 
-	for (; i < height; i ++)
+	size = sizeof(grid) - (sizeof(int *) * height);
+	width = size / sizeof(int);
+	for (j = 0; j < width; j++)
 	{
-		free(grid[i]);
+		free(grid[j]);
 	}
 }
