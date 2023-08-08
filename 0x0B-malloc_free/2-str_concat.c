38ab2d4746590634f93a199e148978ba836caf26
Leul_Mihretu <learnpro1016@gmail.com>
2023-08-08 01:37:10 -0700

    done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,13 +12,20 @@ char *str_concat(char *s1, char *s2)
 	int size;
 	char *newstr;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[size_1] != '\0' || s1 != NULL)
 		size_1++;
 	while (s1[size_2] != '\0' || s2 != NULL)
 		size_2++;
 	size = size_1 + size_2 + 1;
 	newstr = malloc(size - 1);
-
+	if (newstr == NULL)
+	{
+		return (NULL);
+	}
 	while (s1[i] != '\0')
 	{
 		newstr[i] = s1[i];
@@ -30,10 +37,6 @@ char *str_concat(char *s1, char *s2)
 		newstr[size_1] = s2[i];
 		i++;
 		size_1++;
-	}
-	if (newstr == NULL)
-	{
-		return (NULL);
 	}
 	return (newstr);
 }
