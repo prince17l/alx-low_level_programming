9e1e002ec6d9cb74229ea2ce2042b92a6370b25c
Leul_Mihretu <learnpro1016@gmail.com>
2023-08-09 04:40:11 -0700

    done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,19 +9,17 @@
  */
 int main(int argc, char *argv[])
 {
-	int numb1 = atoi(argv[1]);
-	int numb2 = atoi(argv[2]);
-	int multipl;
+	int numb1, numb2;
+	int sum = 0;
 
-	if (argc == 3)
-	{
-		multipl = numb1 * numb2;
-		printf("%d\n", multipl);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	numb1 = atoi(argv[1]);
+	numb2 = atoi(argv[2]);
+	sum = numb1 * numb2;
+	printf("%d\n", sum);
 	return (0);
 }
