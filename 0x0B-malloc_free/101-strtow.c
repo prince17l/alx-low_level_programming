3d61d357affc8dc4dcc38013e3c3aa8a5b363523
Leul_Mihretu <learnpro1016@gmail.com>
2023-08-10 11:19:13 -0700

    first move

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * strtow - it is a main function
+ * @str:  array with type char
+ * Return: char
+ */
+char **strtow(char *str)
+{
+	char **words;
+	
+	if (str == NULL || str == "")
+		return (NULL);
+
+}
