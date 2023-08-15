5b3535055030ffa23973d6039584b7bcf8397f71
Leul_Mihretu <learnpro1016@gmail.com>
2023-08-15 12:24:14 -0700

    done

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
 *  print_dog - this is a main function.
@@ -7,6 +8,8 @@
 */
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		exit;
 	if (d->name == NULL)
 		d->name = "(nil)";
 	if (d->owner == NULL)
