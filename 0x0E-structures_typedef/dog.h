0a92c5901ba70abeda896d3f8ebe7d710417e083
Leul_Mihretu <learnpro1016@gmail.com>
2023-08-15 05:37:27 -0700

    done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H_
+#define DOG_H_
+/**
+ * struct dog - my new struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: there is no long description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
