d6249d62075790141b9733ac4949d6c3188a2218
Leul_Mihretu <learnpro1016@gmail.com>
2023-08-15 13:01:01 -0700

    done

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+*  new_dog - this is a main function.
+*  @name: string
+*  @age: float
+*  @owner: string
+*  Return: void
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+*  free_dog - this is a main function.
+*  @d: struct
+*  Return: void
+*/
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
