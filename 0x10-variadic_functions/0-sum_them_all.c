0a4eb83a778792cb62342e98359a0f6200d6679c
Leul_Mihretu <learnpro1016@gmail.com>
2023-08-17 08:15:06 -0700

    done

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+*  sum_them_all - this is a main function.
+*  @n: variable with type int
+*  Return: integer
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	va_list pointer;
+
+	if (n == 0)
+		return (0);
+
+	va_start(pointer, n);
+	for (int i = 0; i < n; i++)
+	{
+		sum += va_arg(pointer, int);
+	}
+	va_end(pointer);
+	return (sum);
+}
