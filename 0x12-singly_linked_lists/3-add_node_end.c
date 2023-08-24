25d0ae10f6835b0f3607dd930cafb85ac73a40a1
Leul_Mihretu <learnpro1016@gmail.com>
2023-08-24 16:12:49 -0700

    done

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,24 +11,31 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new;
-	list_t *last = *head;
-	unsigned int count;
+	list_t *temp = *head;
+	unsigned int len = 0;
 
-	while (str[count])
-		count++;
+	while (str[len])
+		len++;
+
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
+
 	new->str = strdup(str);
-	new->len = count;
+	new->len = len;
 	new->next = NULL;
+
 	if (*head == NULL)
 	{
 		*head = new;
 		return (new);
 	}
-	while (last->next)
-		last = last->next;
-	last->next = new;
+
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = new;
+
 	return (new);
+
 }
