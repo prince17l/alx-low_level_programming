6c48630f229be1769fc99333cdfccdf2d9d99771
Leul_Mihretu <learnpro1016@gmail.com>
2023-08-24 13:47:48 -0700

    done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,12 +12,12 @@ size_t print_list(const list_t *h)
 
 	while (temp != NULL)
 	{
+		temp = temp->next;
 		if (temp->str == NULL)
 		{
 			printf("[0] (nil)");
 		}
 		printf("[%u] %s", temp->len, temp->str);
-		temp = temp->next;
 		count++;
 	}
 	return (count);
