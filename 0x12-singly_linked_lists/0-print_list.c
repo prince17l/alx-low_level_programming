2381957c4725c1ef442c8484f3e7489685fefd23
Leul_Mihretu <learnpro1016@gmail.com>
2023-08-24 07:38:50 -0700

    done

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - will print the ech element of the list
+ * @h: element of the list
+ * Return: size_t
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	list_t *temp = (list_t *)h;
+
+	while (temp != NULL)
+	{
+		if (temp->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		printf("[%u] %s", temp->len, temp->str);
+
+		count++;
+	}
+	return (count);
+}
