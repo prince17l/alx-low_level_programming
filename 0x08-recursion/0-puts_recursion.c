b386bf9f9089ec6f1dd2c4da0e8b992e7def5385
Leul_Mihretu <learnpro1016@gmail.com>
2023-08-30 22:06:20 +0300

    0-done

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,16 +6,11 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-		_puts_recursion(s);
-	}
-	else
+	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
---
+++ 0x08-recursion/test.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+int main(){
+	_puts_recursion("Leul Mihretu");
+}
