32d0d5c1678587ed5c6095e720be7f042e976d5e
Leul_Mihretu <learnpro1016@gmail.com>
2023-08-30 23:22:14 +0300

    1-done

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _puts_rev_recursion - will print a string.
+ * _print_rev_recursion - will print a string.
  * @s: a variable with type string
  * Return: void
  */
-void _puts_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_puts_rev_recursion(s + 1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 	else
