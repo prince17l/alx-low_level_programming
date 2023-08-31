a585fff162609a606038f94bb6beee26f4a0840a
Leul_Mihretu <learnpro1016@gmail.com>
2023-08-31 14:12:59 +0300

    4-done

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,6 +13,6 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (y > 0)
 		powered = powered * _pow_recursion(x, y - 1);
-	if (y == 0)
 		return (powered);
+	return (powered);
 }
---
+++ 0x08-recursion/test.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdio.h>
+int main(){
+	int result = factorial(3);
+	printf("%d", result);
+	return 0;
+}
