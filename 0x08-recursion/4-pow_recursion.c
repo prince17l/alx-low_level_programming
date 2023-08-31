f764021cbcce7507366bb3004d8bac4a81c853fe
Leul_Mihretu <learnpro1016@gmail.com>
2023-08-31 14:55:07 +0300

    4-done

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,14 +7,10 @@
  */
 int _pow_recursion(int x, int y)
 {
-	int powered = x;
-
 	if (y < 0)
 		return (-1);
-	if (y == 0)
-		return (powered);
-	powered = powered * x;
-	_pow_recursion(x, y - 1);
-	return (powered);
+	if (y == 0 || y == 1)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
 
 }
