d092c5f3d1ed05fa4a505ff26d8eb4c3c546ef66
Leul_Mihretu <learnpro1016@gmail.com>
2023-09-02 00:33:39 +0300

    diractory

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - will return x square root of n.
+ * @n: a variable with type int
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	int root = 0;
+	int root = n;
+	if (root != dup)
+}
--- 0x08-recursion/test.c
+++ 0x08-recursion/test.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 int main(){
-	int result = factorial(3);
+	int result = _pow_recursion(5, 3);
 	printf("%d", result);
 	return 0;
 }
