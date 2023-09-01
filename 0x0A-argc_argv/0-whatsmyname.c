7bac6fc6660ae7e0c30b053d3a48409664895d55
Leul_Mihretu <learnpro1016@gmail.com>
2023-09-02 00:32:55 +0300

    0-done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - is a main function
+ * @argc: size of argv
+ * @argv: command line arguments
+ * Return: int
+ */
+int main(int argc, char *argv)
+{
+	printf("%s", arhc[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This project is about argv and argc
