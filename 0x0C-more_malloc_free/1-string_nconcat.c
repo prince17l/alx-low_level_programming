4f55fc34d448159a8e3118a1e0fc5eda7a50295c
Leul_Mihretu <learnpro1016@gmail.com>
2023-09-11 22:02:08 +0300

    first-move

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * string_nconcat - is the main function
+ * @n: is unsigned int
+ * @s1: string
+ * @s2: string
+ * Return: char array
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *newstr;
+
+}
